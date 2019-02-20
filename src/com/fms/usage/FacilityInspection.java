0a06b7a1d97d10088367bcafba566561dfe54808
jconroy5 <jconroy5@luc.edu>
2019-02-20 15:10:23 -0600

    propagated FacilityInspection

--- src/com/fms/usage/FacilityInspection.java
+++ src/com/fms/usage/FacilityInspection.java
@@ -2,4 +2,30 @@ package com.fms.usage;
 
 public class FacilityInspection {
 
+    //vars for Facility ID, type of inspection, and optional details of an inspection
+    private int facility_ID;
+    private String inspection_type;
+    private String inspection_detail;
+
+    public FacilityInspection() {}
+
+    //getters and setters
+    public String getInspection_type() {
+        return inspection_type;
+    }
+    public void setInspection_type(String inspection_type) {
+        this.inspection_type = inspection_type;
+    }
+    public String getInspection_detail() {
+        return inspection_detail;
+    }
+    public void setInspection_detail(String inspection_detail) {
+        this.inspection_detail = inspection_detail;
+    }
+    public int getFacility_ID() {
+        return facility_ID;
+    }
+    public void setFacility_ID(int facility_ID) {
+        this.facility_ID = facility_ID;
+    }
 }
