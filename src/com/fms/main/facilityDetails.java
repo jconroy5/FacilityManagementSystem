f777176c78e38a13c7e50abae4f2308b391b5dc6
jconroy5 <jconroy5@luc.edu>
2019-02-20 12:57:03 -0600

    updated main facilityDetails; name, location, phone, numberOfRooms

--- src/com/fms/main/facilityDetails.java
+++ src/com/fms/main/facilityDetails.java
@@ -1,5 +1,35 @@
 package com.fms.main;
 
-public class facilityDetails {
+public class facilityDetails extends Facility {
+    private String name;
+    private String location;
+    private int phoneNumber;
+    private int numberOfRooms;
 
+    public facilityDetails(){}
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public void setLocation(){
+        this.location = location;
+    }
+    public int getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+    public void setNumberOfRooms(int numberOfRooms){
+        this.numberOfRooms = numberOfRooms;
+    }
 }
