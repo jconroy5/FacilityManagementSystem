82baf9b42304ea65445ac07e8d20bdc344eb0f24
jconroy5 <jconroy5@luc.edu>
2019-02-20 15:45:16 -0600

    added method isInUseDuringInterval and reference comment

--- src/com/fms/DAL/UsageDAO.java
+++ src/com/fms/DAL/UsageDAO.java
@@ -12,18 +12,26 @@ import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Modeled after CustomerDAO.java from BookStore, COMP 373/473, Spring 2019.
+ */
 public class UsageDAO {
 
     public UsageDAO() {}
 
-    public List<FacilityInspection> listInspections(Facility fac) {
+    /**
+     * Generates a list of inspections at a given Facility
+     * @param facility the facility that will be searched for inspections
+     * @return returns listOfInspections
+     */
+    public List<FacilityInspection> listInspections(Facility facility) {
 
         List<FacilityInspection> listOfInspections = new ArrayList<FacilityInspection>();
 
         try {
             Statement st = DBHelper.getConnection().createStatement();
             String listInspectionsQuery = "SELECT * FROM inspection WHERE "
-                    + "facility_id = '" + fac.getFacilityID() + "'";
+                    + "facility_id = '" + facility.getFacilityID() + "'";
 
             ResultSet useRS = st.executeQuery(listInspectionsQuery);
             System.out.println("UseDAO: ********** Query " + listInspectionsQuery + "\n");
@@ -32,7 +40,7 @@ public class UsageDAO {
                 FacilityInspection inspec = new FacilityInspection();
                 inspec.setInspection_type(useRS.getString("inspection_type"));
                 inspec.setInspection_detail(useRS.getString("inspection_detail"));
-                inspec.setFacility_ID(fac.getFacilityID());
+                inspec.setFacility_ID(facility.getFacilityID());
                 listOfInspections.add(inspec);
             }
 
@@ -47,5 +55,47 @@ public class UsageDAO {
             se.printStackTrace();
         }
         return listOfInspections;
+    }
+
+    /**
+     * Checks if given Facility is in use given a specific interval of time, by room number
+     * @param facilityUse
+     * @return Returns true if a Facility or room is in use, returns false otherwise
+     */
+    public boolean isInUseDuringInterval(FacilityUse facilityUse) {
+
+        //default is false
+        boolean result = false;
+        try {
+            //Takes in Facility ID, room number, and start and end dates into use table
+            Statement st = DBHelper.getConnection().createStatement();
+            String selectUseAssignments = "SELECT * FROM use WHERE facility_id = " + facilityUse.getFacilityID() +
+                    " AND room_number IN (0, " + facilityUse.getRoomNumber() + ")";
+
+            ResultSet useRS = st.executeQuery(selectUseAssignments);
+            System.out.println("UseDAO: ********** Query " + selectUseAssignments + "\n");
+
+            //checks if the DB dates overlap with the input interval
+            while (useRS.next()) {
+                LocalDate assignStart = useRS.getDate("start_date").toLocalDate();
+                LocalDate assignEnd = useRS.getDate("end_date").toLocalDate();
+                if (facilityUse.getStartDate().isBefore(assignEnd) && (assignStart.isBefore(facilityUse.getEndDate()) ||
+                        assignStart.equals(facilityUse.getEndDate()))) {
+                    result = true;
+                    break;
+                }
+            }
+
+            //close to manage resources
+            useRS.close();
+            st.close();
+        }
+        catch (SQLException se) {
+            System.err.println("UseDAO: Threw a SQLException checking if "
+                    + "facility is in use during an interval.");
+            System.err.println(se.getMessage());
+            se.printStackTrace();
+        }
+        return result;
     }
 }
