5cd6e5694834faae4780a3b31a0893f03e204c12
jconroy5 <jconroy5@luc.edu>
2019-02-20 18:54:16 -0600

    added methods addNewFacility and getFacilityInformation

--- src/com/fms/services/FacilityService.java
+++ src/com/fms/services/FacilityService.java
@@ -1,5 +1,35 @@
 package com.fms.services;
 
+import com.fms.DAL.FacilityDAO;
+import com.fms.DAL.UsageDAO;
+import com.fms.usage.FacilityUse;
+import com.fms.main.Facility;
+
 public class FacilityService {
 
+    private FacilityDAO facDAO = new FacilityDAO();
+    private UsageDAO useDAO = new UsageDAO();
+
+    //Add new Facility to DB
+    public void addNewFacility(Facility facility) {
+        try {
+            facDAO.addNewFacility(facility);
+        } catch (Exception se) {
+            System.err.println("FacilityService: Threw an Exception adding new facility.");
+            System.err.println(se.getMessage());
+        }
+    }
+
+    //gets Facility's info
+    public Facility getFacilityInformation(int id) {
+
+        try {
+            Facility facility = facDAO.getFacilityInformation(id);
+            return facility;
+        } catch (Exception se) {
+            System.err.println("FacilityService: Threw an Exception retrieving facility.");
+            System.err.println(se.getMessage());
+        }
+        return null;
+    }
 }
