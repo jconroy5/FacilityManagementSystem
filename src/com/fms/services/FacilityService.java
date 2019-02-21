62f99210708782fca346036587a817e4dfa42259
jconroy5 <jconroy5@luc.edu>
2019-02-20 18:57:20 -0600

    added methods removeFacility, addFacilityDetail, and listFacilities

--- src/com/fms/services/FacilityService.java
+++ src/com/fms/services/FacilityService.java
@@ -4,6 +4,8 @@ import com.fms.DAL.FacilityDAO;
 import com.fms.DAL.UsageDAO;
 import com.fms.usage.FacilityUse;
 import com.fms.main.Facility;
+import java.time.LocalDate;
+import java.util.List;
 
 public class FacilityService {
 
@@ -28,6 +30,38 @@ public class FacilityService {
             return facility;
         } catch (Exception se) {
             System.err.println("FacilityService: Threw an Exception retrieving facility.");
+            System.err.println(se.getMessage());
+        }
+        return null;
+    }
+
+    //removes Facility listing from DB by ID
+    public void removeFacility(int id) {
+
+        try {
+            facDAO.removeFacility(id);
+        } catch (Exception se) {
+            System.err.println("FacilityService: Threw an Exception removing facility.");
+            System.err.println(se.getMessage());
+        }
+    }
+
+    //adds phone number to a Facility listing in DB by Facility ID
+    public void addFacilityDetail(int ID, int phoneNumber) {
+        try {
+            facDAO.addFacilityDetail(ID, phoneNumber);
+        } catch (Exception se) {
+            System.err.println("FacilityService: Threw an Exception updating phone in facility_detail.");
+            System.err.println(se.getMessage());
+        }
+    }
+
+    //lists available Facilities
+    public List<Facility> listFacilities() {
+        try {
+            return facDAO.listFacilities();
+        } catch (Exception se) {
+            System.err.println("FacilityService: Threw an Exception retrieving list of facilities.");
             System.err.println(se.getMessage());
         }
         return null;
