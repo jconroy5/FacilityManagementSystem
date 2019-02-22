90b560019b17e83cf645b6bad2025771635507c0
jconroy5 <jconroy5@luc.edu>
2019-02-22 17:32:19 -0600

    test

--- src/com/fms/client/ClientInterface.java
+++ src/com/fms/client/ClientInterface.java
@@ -2,12 +2,13 @@ package com.fms.client;
 
 public class ClientInterface {
     /**
-     * Select one of the client interfaces below to run by commenting out the other two
+     * Select one of the client interfaces below to run by commenting out the other two.
      *
      * EXAMPLE - RUNNING MaintenanceClient:
      *      //FacilityClient facilityClient = new FacilityClient();
      *      //UsageClient usageClient = new UsageClient();
      *      MaintenanceClient maintenanceClient = new MaintenanceClient();
+     *
      */
     public static void main(String[] args) throws Exception {
 
