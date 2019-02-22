1f2d3c6fde6e3998ca92dc22f058819c3a998fe9
jconroy5 <jconroy5@luc.edu>
2019-02-22 13:56:34 -0600

    added method isInUseDuringInterval and some missing comments

--- src/com/fms/services/UsageService.java
+++ src/com/fms/services/UsageService.java
@@ -9,6 +9,7 @@ public class UsageService {
 
     private UsageDAO usageDAO = new UsageDAO();
 
+    //generates a list of inspections at a given Facility
     public List<FacilityInspection> listInspections(Facility facility) {
         try {
             return usageDAO.listInspections(facility);
@@ -17,5 +18,26 @@ public class UsageService {
             System.err.println(se.getMessage());
         }
         return null;
+    }
+
+    //checks if a Facility is in use during a given interval
+    public boolean isInUseDuringInterval(FacilityUse facilityUse) {
+        //checks for a valid start date
+        if (facilityUse.getStartDate().isAfter(facilityUse.getEndDate())) {
+            System.out.println("The start date cannot be after the end date.");
+            //checks for a valid number of rooms
+        } else if (facilityUse.getRoomNumber() > facilityUse.getDetails().getNumberOfRooms()) {
+            System.out.println("Invalid room number. There are only " +
+                    facilityUse.getDetails().getNumberOfRooms() +
+                    " rooms at this facility.");
+        } else {
+            try {
+                return usageDAO.isInUseDuringInterval(facilityUse);
+            } catch (Exception se) {
+                System.err.println("UseService: Threw an Exception checking if facility is in use during interval.");
+                System.err.println(se.getMessage());
+            }
+        }
+        return true;
     }
 }
