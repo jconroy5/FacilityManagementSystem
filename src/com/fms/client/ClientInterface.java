7ea99dff59b6de24f4906a9b8fccfabb5a1b917a
jconroy5 <jconroy5@luc.edu>
2019-02-22 15:25:17 -0600

    added main method

--- src/com/fms/client/ClientInterface.java
+++ src/com/fms/client/ClientInterface.java
@@ -4,4 +4,10 @@ public class ClientInterface {
     /**
      * Select one of the client interfaces below to run by commenting out the other two
      */
+    public static void main(String[] args) throws Exception {
+
+        FacilityClient facilityClient = new FacilityClient();
+        //UsageClient usageClient = new UsageClient();
+        //MaintenanceClient maintenanceClient = new MaintenanceClient();
+    }
 }
