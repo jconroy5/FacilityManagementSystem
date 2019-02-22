442b6a83c82b3b856d6496ebc9b9139f4ef3f912
jconroy5 <jconroy5@luc.edu>
2019-02-22 14:02:22 -0600

    added methods listActualUsage and assignFacilityToUse

--- src/com/fms/services/UsageService.java
+++ src/com/fms/services/UsageService.java
@@ -40,4 +40,38 @@ public class UsageService {
         }
         return true;
     }
+
+    //generates list of usage at a given Facility
+    public List<FacilityUse> listActualUsage(Facility facility) {
+        try {
+            return usageDAO.listActualUsage(facility);
+        } catch (Exception se) {
+            System.err.println("UseService: Threw an Exception retrieving list of usage.");
+            System.err.println(se.getMessage());
+        }
+        return null;
+    }
+
+    //assigns a room number at a given Facility to use
+    public void assignFacilityToUse(FacilityUse facilityUse) {
+        //check if the start date is valid
+        if (facilityUse.getStartDate().isAfter(facilityUse.getEndDate())) {
+            System.out.println("The start date cannot be after the end date.");
+            //check if the room number is valid
+        } else if (facilityUse.getRoomNumber() > facilityUse.getDetails().getNumberOfRooms()) {
+            System.out.println("Invalid room number. There are only " +
+                    facilityUse.getDetails().getNumberOfRooms() +
+                    " rooms at this facility.");
+            //check if given room is already in use during interval
+        } else if (isInUseDuringInterval(facilityUse)) {
+            System.out.println("This room is already in use during this interval.");
+        } else {
+            try {
+                usageDAO.assignFacilityToUse(facilityUse);
+            } catch (Exception se) {
+                System.err.println("UseService: Threw an Exception assigning a facility to use.");
+                System.err.println(se.getMessage());
+            }
+        }
+    }
 }
